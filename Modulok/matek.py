a8a60a31c6d7c30c53d4bf796aadcfecfc41826c
kassa.mate <kassa.mate@ckik.hu>
2024-02-08 10:21:49 +0100

    matek +(rajottem_hogy_hasznos_a_python_extension)

---
+++ Modulok/matek.py
@@ -0,0 +1,16 @@
+from math import * 
+
+# sqrt = négyzetgyök vonás
+print(f"Gyök 2: {sqrt(2)}")
+
+#faktoriális
+print(f"5!: {factorial(5)}")
+
+# comb = kombináció
+print(f"30 alatt a 3: {comb(30, 3)}")
+
+# gcd = greatest sommon divisor(legnagyobb köszös osztó)
+print(f"(20, 24){gcd(20, 24)}")
+
+# pi = pí konstans
+print(f"5 egység sugarú kör kerülete: {5*2*pi}")
