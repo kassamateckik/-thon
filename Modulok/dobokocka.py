4578a1b20bbb9012e1697c258382adc4ebecac14
kassa.mate <kassa.mate@ckik.hu>
2024-02-14 11:11:14 +0100

    jajjmegdoglunk

---
+++ Modulok/dobokocka.py
@@ -0,0 +1,28 @@
+from random import randint
+
+n = int(input("n: "))
+# Ezt ne mert tanár úr kiherél!
+# egy = 0
+# ketto = 0
+# harom = 0
+# negy = 0
+# ot = 0
+# hat = 0
+# for i in range(n):
+#     r = randint(1, 6)
+#     if r == 1:
+#         egy += 1
+#     elif r == 2:
+#         ketto += 1
+#     elif r == 3:
+#         harom += 1
+#     elif r == 4:
+#         negy += 1
+#     elif r == 5:
+#         ot += 1
+#     else:
+#         hat += 1
+# print(f"Egy: {egy}\nKettő: {ketto}\nHárom: {harom}\nNégy: {negy}\nÖt: {ot}\nHat: {hat}")
+
+
+
