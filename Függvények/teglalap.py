5602d66733c534357ffebb3ba100e9bea12b43e5
kassa.mate <kassa.mate@ckik.hu>
2024-02-14 12:24:49 +0100

    fuggvenyeeeeeek

---
+++ Függvények/teglalap.py
@@ -0,0 +1,26 @@
+# Függvény:
+# gép, ami f(x)= x*x
+# paramétereket kap: 3 
+# visszatérési értéket ad: 9
+
+def terulet(a, b): # függvény fejléc
+    # függvény törzs:
+    # pass
+    return a*b
+
+def kerulet(a, b):
+    return 2*(a+b)
+
+# ---------------------------------------------
+
+print(terulet(3, 7))
+print(terulet(2, 13))
+print(terulet(12, 20))
+print(terulet(5, 8))
+
+print("-----------")
+
+print(kerulet(3, 7))
+print(kerulet(2, 13))
+print(kerulet(12, 20))
+print(kerulet(5, 8))
