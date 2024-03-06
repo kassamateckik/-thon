47e945ac0f6faebb48f96afa07eacb8ac8445197
kassa.mate <kassa.mate@ckik.hu>
2024-03-06 12:25:14 +0100

    akasztofaszerintemkesz

--- Gyakorlás/akasztofa.py
+++ Gyakorlás/akasztofa.py
@@ -1,5 +1,6 @@
 from random import randrange
 from os import system
+from tkinter import N
 
 def sorsolas():
     szavak = ["hálózat", "python", "számítógép", "értelmes", "szavak", "dolgozat"]
@@ -23,14 +24,42 @@ def csere(betu, aktualis, megoldas):
         if betu == megoldas[i]:
             aktualis[i] = betu
 
+def benne(a, lista):
+    i = 0
+    while i < len(lista) and lista[i] != a:
+        i += 1
+    return i < len(lista)    
+
+def fordulo(aktualis, megoldas, hibak):
+    betu = input("\nBetű: ")
+    if len(betu) != 1:
+        return
+    if not benne(betu, megoldas) and not benne(betu, hibak):
+        hibak.append(betu)
+    system("cls")
+    csere(betu, aktualis, megoldas)
+    kirajzol(aktualis)
+    print(f"Hibák: {hibak}")
+        
+def vege(hibak):
+    if len(hibak) <= 2:
+        print("Nyertél!")
+    else:
+        print("Vesztettél!")
+
+def jatek(megoldas):
+    aktualis = kezdeti_allapot(len(megoldas))
+    kirajzol(aktualis)
+    hibak = []
+    while benne("_", aktualis) and len(hibak) < 3:  # aktualis != list(megoldas)
+        fordulo(aktualis, megoldas, hibak)
+    vege(hibak)
+
 
 def main():
     system("cls")
     megoldas = sorsolas()
-    aktualis = kezdeti_allapot(len(megoldas))
-    print(megoldas)
-    kirajzol(aktualis)
-    betu = input("\nBetű: ")
-    csere(betu, aktualis, megoldas)
+    print(megoldas) # magunknak csalunk
+    jatek(megoldas)
     
 main()
