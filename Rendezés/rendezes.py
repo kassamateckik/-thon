b2844ecf31f226832807233bc4d738e6f119f89b
kassa.mate <kassa.mate@ckik.hu>
2024-03-13 08:45:13 +0100

    csokkenorendez, nagyrandomlista

--- Rendezés/rendezes.py
+++ Rendezés/rendezes.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def csere(x, i, j):
     a = x[i]
     x[i] = x[j]
@@ -10,14 +12,44 @@ def minindex(x, n=0):
             mini = i
     return mini
 
-def rendez(x):
-    for i in range(len(x)):
-        j = minindex(x, i)
-        csere(x, i, j)
+def maxindex(x, n=0):
+    mini = n
+    for i in range(n+1, len(x)):
+        if x[i] > x[mini]:
+            mini = i
+    return mini
+
+def rendez(x, jel):
+    if jel == "+":
+        for i in range(len(x)):
+            j = minindex(x, i)
+            csere(x, i, j)
+    else:
+        for i in range(len(x)):
+            j = maxindex(x, i)
+            csere(x, i, j)
+
+def feltolt(n):
+    x = []
+    for i in range(n):
+        r = randint(1, 9)
+        x.append(r)
+    return x
+
 
 def main():
     x =[5, 2, 7, 1, 2, 3]
-    rendez(x)
+    rendez(x, "+")
     print(x)
+    rendez(x, "-")
+    print(x)
+
+    x = feltolt(100)
+    print(x)
+    rendez(x, "+")
+    print(x)
+    rendez(x, "-")
+    print(x)
+
 
 main()
